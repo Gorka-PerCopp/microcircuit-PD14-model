d2f52d13ca83a07fd3850dc8307eebf101f0d603
g.peraza.coppola <g.peraza.coppola@fz-juelich.de>
2025-10-02 14:01:57 +0200

    Edited analysis script run for each seed separately + script to compute ensamble statistics

--- PyNEST/reference_data/compute_ensamble_statistics.py
+++ PyNEST/reference_data/compute_ensamble_statistics.py
@@ -41,22 +41,6 @@ from microcircuit.stimulus_params import default_stim_dict as stim_dict
 ## import analysis parameters
 from params import params as ref_dict
 
-from pathlib import Path
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument("--seed", type=int, default=12345)
-parser.add_argument("--path", type=str, default="data")
-args = parser.parse_args()
-
-path = Path(args.path)
-sim_dict.update(
-        {
-            "data_path": str(path) + "/",
-            "rng_seed": args.seed,
-        }
-)
-
 #####################
 populations = net_dict['populations'] # list of populations
 #####################
@@ -68,62 +52,65 @@ net_dict["K_scaling"] = scaling_factor
 
 random.seed( ref_dict['seed_subsampling'] )  # set seed for reproducibility
 
+seeds = ref_dict['RNG_seeds'] # list of seeds
+
+sim_dict['data_path'] = 'data_T' + str( int( ref_dict['t_sim'] * 1.0e-3 ) ) + 's/'
+
 ########################################################################################################################
 #                                   Define auxiliary functions to analyze and plot data                                #
 ########################################################################################################################
+def concatenate_data( observable_name ):
+    observable = {}
 
-def analyze_single_neuron_stats(observable_name, func):
-    observable = {} # list of single neuron observable [pop][neuron]
-    recording_interval = ( max( ref_dict['t_min'], ref_dict['t_presim'] ), ref_dict['t_presim'] + ref_dict['t_sim'] )
+    for cseed, seed in enumerate( seeds ):
+        observable[cseed] = {}
+        data_path = sim_dict['data_path'] + 'seed-%s/' % seed
 
+        # load data per seed as dictionary sorted by populations
+        data_per_seed = helpers.json2dict( f'{data_path}{observable_name}.json' )
+
+        # concatenate data
+        observable[cseed] = data_per_seed
     
-    data_path = sim_dict['data_path']
-    nodes = helpers.json2dict( data_path + 'nodes.json' )
-
-    for pop in populations:
-        observable[pop] = {}
-
-        label = 'spike_recorder-' + str( nodes['spike_recorder_%s' % pop][0] )
-        spikes = helpers.load_spike_data( data_path, label )
-        observable[pop] = list( func( spikes, nodes[pop], recording_interval ) )
-
-    # store observable as json file
+    # store concatenated data as json file
     helpers.dict2json( observable, sim_dict['data_path'] + f'{observable_name}.json' )
 
     return observable
 
-def analyze_pairwise_stats( observable_name, func ):
+def compute_ks_distances( observable, observable_name ):
+    observable_ks_distances = {} # list of ks distances [pop][seed][ks_distance to other seed]
+    for cpop, pop in enumerate( populations ):
+        observable_ks_distances[pop] = {
+            "seeds": {},    # values across seeds
+            "list": []      # to compute mean and std
+        }
 
-    recording_interval = ( max ( ref_dict['t_min'], ref_dict['t_presim'] ), ref_dict['t_presim'] + ref_dict['t_sim'] )
+        # clean data: remove NaNs
+        for cseed, seed in enumerate( seeds ):
+            observable[cseed][pop] = np.delete( observable[cseed][pop], np.where( np.isnan( observable[cseed][pop] ) ) )
 
-    #cc_binsize = 2. # in ms
-    observable = {}  # list of pairwise spike count correlations [pop][correlation]
-    
-    data_path = sim_dict['data_path']
-    nodes = helpers.json2dict( data_path + 'nodes.json' )
-    
-    for pop in populations:
-        observable[pop] = {}
+        n_seeds = len( seeds ) 
 
-        pop_nodes = nodes[pop]  # list of neuron nodes for the population
-        label = 'spike_recorder-' + str( nodes['spike_recorder_%s' % pop][0] )
-        spikes = helpers.load_spike_data( data_path, label )
+        for i in range( n_seeds ):
+            observable_ks_distances[pop]["seeds"][i] = {}
+            for j in range( i+1, n_seeds ):
+                observable_ks_distance = ks( observable[i][pop], observable[j][pop] )[0].tolist()
+                observable_ks_distances[pop]["seeds"][i][j] = observable_ks_distance
+                observable_ks_distances[pop]["list"].append( observable_ks_distance )
 
-        # Get random indices without replacement
-        selected_nodes = random.sample( pop_nodes, ref_dict['subsample_size'] )
+    # save ks distances as json file
+    helpers.dict2json( observable_ks_distances, sim_dict['data_path'] + f'{observable_name}_ks_distances.json' )
 
-        observable[pop] = list( func( spikes, selected_nodes, recording_interval, ref_dict['binsize'] ) )
-
-    # store pairwise spike count correlations as json file
-    helpers.dict2json( observable, sim_dict['data_path'] + f'{observable_name}.json' )
-
-    return observable
+    return observable_ks_distances
 
 def main():
-
-    analyze_single_neuron_stats( 'rates', helpers.time_averaged_single_neuron_firing_rates )
-    analyze_single_neuron_stats( 'spike_cvs', helpers.single_neuron_isi_cvs )
-    analyze_pairwise_stats( 'spike_ccs', helpers.pairwise_spike_count_correlations )
+    rates = concatenate_data( 'rate' )
+    spike_cvs = concatenate_data( 'spike_cvs' )
+    spike_ccs = concatenate_data( 'spike_ccs' )
+    
+    compute_ks_distances( rates, 'rate' )
+    compute_ks_distances( spike_cvs, 'spike_cvs' )
+    compute_ks_distances( spike_ccs, 'spike_ccs' )
 
     ## current memory consumption of the python process (in MB)
     import psutil
